59bccdee82231cf93f66a020fbd5947528dec6b5
godzie44 <godzie@yandex.ru>
2021-01-14 13:09:07 +0300

    add radix sort

---
+++ radixsort/sort.go
@@ -0,0 +1,32 @@
+package radixsort
+
+const (
+	byteMask = 0xFF
+)
+
+func Sort(arr []int32) []int32 {
+	for i := 0; i < 4; i++ {
+		arr = cSort(arr, i)
+	}
+
+	return arr
+}
+
+func cSort(arr []int32, byteNumber int) []int32 {
+	c := make([][]int32, 256)
+
+	shift := byteNumber * 8
+	for _, v := range arr {
+		byteVal := byte(v & (byteMask << shift) >> shift)
+		c[byteVal] = append(c[byteVal], v)
+	}
+
+	result := make([]int32, 0, len(arr))
+	for _, values := range c {
+		for _, v := range values {
+			result = append(result, v)
+		}
+	}
+
+	return result
+}
---
+++ radixsort/sort_test.go
@@ -0,0 +1,27 @@
+package radixsort
+
+import (
+	"algorithms"
+	"github.com/stretchr/testify/assert"
+	"math"
+	"sort"
+	"testing"
+)
+
+func TestSort(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		arr := algorithms.GenerateTopBoundedRandomSlice(t, math.MaxInt32)
+		int32Arr := make([]int32, len(arr))
+		for i := range arr {
+			int32Arr[i] = int32(arr[i])
+		}
+		res := Sort(int32Arr)
+
+		intRes := make([]int, len(res))
+		for i := range res {
+			intRes[i] = int(res[i])
+		}
+
+		assert.True(t, sort.IntsAreSorted(intRes))
+	}
+}

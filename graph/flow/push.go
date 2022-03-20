9c4f476e736f4c4e38f55e10be0c3dca9ea45849
godzie44 <godzie@yandex.ru>
2022-03-21 01:17:45 +0300

    graph: add relabel to front algorithm

--- graph/flow/push.go
+++ graph/flow/push.go
@@ -2,28 +2,28 @@ package flow
 
 import (
 	"algorithms/graph"
+	"container/list"
 	"fmt"
 	"math"
 	"sort"
 )
 
-type (
-	context struct {
-		e map[*graph.V]int
-		h map[*graph.V]int
-		f map[graph.Edge]int
-		c map[graph.Edge]int
-	}
-	algo struct {
-		g   *graph.G
-		ctx context
-	}
-)
+type context struct {
+	g *graph.G
+	e map[*graph.V]int
+	h map[*graph.V]int
+	f map[graph.Edge]int
+	c map[graph.Edge]int
+}
 
-func newAlgo(g *graph.G, s *graph.V) *algo {
-	a := &algo{
-		g: g,
+type relabelMaxFlowAlgo struct {
+	ctx context
+}
+
+func newAlgo(g *graph.G, s *graph.V) *relabelMaxFlowAlgo {
+	a := &relabelMaxFlowAlgo{
 		ctx: context{
+			g: g,
 			e: map[*graph.V]int{},
 			h: map[*graph.V]int{},
 			f: map[graph.Edge]int{},
@@ -31,31 +31,14 @@ func newAlgo(g *graph.G, s *graph.V) *algo {
 		},
 	}
 
-	for e, w := range a.g.Weights {
-		a.ctx.c[e] = w
-		a.ctx.f[e] = 0
-		a.ctx.f[graph.Edge{e.V2, e.V1}] = 0
-	}
-
-	for _, vert := range g.Vertexes {
-		a.ctx.h[vert] = 0
-		a.ctx.e[vert] = 0
-	}
-
-	for _, u := range a.g.Adj[s] {
-		a.ctx.f[graph.Edge{s, u}] = a.ctx.c[graph.Edge{s, u}]
-		a.ctx.f[graph.Edge{u, s}] = -a.ctx.c[graph.Edge{s, u}]
-		a.ctx.e[u] = a.ctx.c[graph.Edge{s, u}]
-		a.ctx.e[s] -= a.ctx.c[graph.Edge{s, u}]
-	}
-	a.ctx.h[s] = len(g.Vertexes)
+	initPreflow(&a.ctx, g, s)
 	return a
 }
 
-func (a *algo) do(s, t *graph.V) map[graph.Edge]int {
+func (a *relabelMaxFlowAlgo) do(s, t *graph.V) map[graph.Edge]int {
 	for {
 		var u *graph.V
-		for _, v := range a.g.Vertexes {
+		for _, v := range a.ctx.g.Vertexes {
 			if v == s || v == t {
 				continue
 			}
@@ -69,52 +52,51 @@ func (a *algo) do(s, t *graph.V) map[graph.Edge]int {
 			break
 		}
 
-		less, _ := a.children(u)
-		if len(less) == 0 {
-			a.relabel(u)
+		var vert *graph.V
+		for _, v := range a.ctx.g.Vertexes {
+			if a.ctx.c[graph.Edge{u, v}]-a.ctx.f[graph.Edge{u, v}] > 0 && a.ctx.h[u] == a.ctx.h[v]+1 {
+				vert = v
+				break
+			}
+		}
+
+		if vert != nil {
+			push(&a.ctx, u, vert)
 		} else {
-			a.push(u, less[len(less)-1])
+			relabel(&a.ctx, u)
 		}
 	}
 
 	return a.ctx.f
 }
 
-func (a *algo) children(u *graph.V) (less []*graph.V, notLess []*graph.V) {
-	for _, v := range a.g.Vertexes {
-		if _, exists := a.ctx.f[graph.Edge{u, v}]; exists {
-			if a.ctx.h[v] < a.ctx.h[u] {
-				less = append(less, v)
-			} else {
-				notLess = append(notLess, v)
-			}
+func children(ctx *context, u *graph.V) (c []*graph.V) {
+	for _, v := range ctx.g.Vertexes {
+		if ctx.c[graph.Edge{u, v}]-ctx.f[graph.Edge{u, v}] == 0 {
+			continue
 		}
+
+		c = append(c, v)
 	}
-	return less, notLess
+	return c
 }
 
-func (a *algo) push(u, v *graph.V) {
-	d := min(a.ctx.e[u], a.ctx.c[graph.Edge{u, v}]-a.ctx.f[graph.Edge{u, v}])
-	a.ctx.f[graph.Edge{u, v}] += d
-	a.ctx.f[graph.Edge{v, u}] -= d
-	a.ctx.e[u] -= d
-	a.ctx.e[v] += d
-
-	if a.ctx.f[graph.Edge{u, v}] == a.ctx.c[graph.Edge{u, v}] {
-		delete(a.ctx.f, graph.Edge{u, v})
-	}
+func push(ctx *context, u, v *graph.V) {
+	d := min(ctx.e[u], ctx.c[graph.Edge{u, v}]-ctx.f[graph.Edge{u, v}])
+	ctx.f[graph.Edge{u, v}] += d
+	ctx.f[graph.Edge{v, u}] -= d
+	ctx.e[u] -= d
+	ctx.e[v] += d
 }
 
-func (a *algo) relabel(u *graph.V) {
+func relabel(ctx *context, u *graph.V) {
 	var res = math.MaxInt
 
-	var less, notLess = a.children(u)
-
-	for _, v := range append(less, notLess...) {
-		res = min(res, a.ctx.h[v])
+	for _, v := range children(ctx, u) {
+		res = min(res, ctx.h[v])
 	}
 
-	a.ctx.h[u] = res + 1
+	ctx.h[u] = res + 1
 }
 
 func PushRelabelMaxFlow(g *graph.G, s, t *graph.V) map[graph.Edge]int {
@@ -122,23 +104,136 @@ func PushRelabelMaxFlow(g *graph.G, s, t *graph.V) map[graph.Edge]int {
 	return a.do(s, t)
 }
 
-func (a *algo) dump() {
-	for _, v := range a.g.Vertexes {
-		fmt.Println("n: ", v.Val, "h:", a.ctx.h[v], "e:", a.ctx.e[v])
+func dump(ctx *context) {
+	fmt.Println()
+
+	for _, v := range ctx.g.Vertexes {
+		fmt.Println("n: ", v.Val, "h:", ctx.h[v], "e:", ctx.e[v])
 	}
 
 	fmt.Printf("Edge     |  c ->   | flow -> | c <- | flow <- | \n")
 
 	var edges []graph.Edge
-	for e := range a.g.Weights {
+	for e := range ctx.g.Weights {
 		edges = append(edges, e)
 	}
 	sort.Slice(edges, func(i, j int) bool {
 		return edges[i].V1.Val.(string) > edges[j].V1.Val.(string)
 	})
 
-	for e := range a.g.Weights {
+	for e := range ctx.g.Weights {
 		e2 := graph.Edge{e.V2, e.V1}
-		fmt.Printf("%s -> %s  |  %02d     |  %02d     |  %02d  |  %d    | \n", e.V1.Val, e.V2.Val, a.ctx.c[e], a.ctx.f[e], a.ctx.c[e2], a.ctx.f[e2])
+		fmt.Printf("%s -> %s  |  %02d     |  %02d     |  %02d  |  %d    | \n", e.V1.Val, e.V2.Val, ctx.c[e], ctx.f[e], ctx.c[e2], ctx.f[e2])
 	}
+}
+
+type relabelToFrontAlgo struct {
+	g    *graph.G
+	allN map[*graph.V]*neighbours
+	ctx  context
+}
+
+func RelabelToFront(g *graph.G, s, t *graph.V) map[graph.Edge]int {
+	alg := newRelableToFrontAlgo(g, s, t)
+	return alg.do(s, t)
+}
+
+func newRelableToFrontAlgo(g *graph.G, s, t *graph.V) *relabelToFrontAlgo {
+	alg := &relabelToFrontAlgo{
+		g: g,
+		ctx: context{
+			g: g,
+			e: map[*graph.V]int{},
+			h: map[*graph.V]int{},
+			f: map[graph.Edge]int{},
+			c: map[graph.Edge]int{},
+		},
+		allN: map[*graph.V]*neighbours{},
+	}
+	initPreflow(&alg.ctx, g, s)
+
+	for _, v := range g.Vertexes {
+		alg.allN[v] = &neighbours{}
+
+		for _, j := range g.Vertexes {
+			_, ex1 := alg.ctx.f[graph.Edge{v, j}]
+			_, ex2 := alg.ctx.f[graph.Edge{j, v}]
+			if ex1 || ex2 {
+				alg.allN[v].n = append(alg.allN[v].n, j)
+			}
+		}
+	}
+
+	return alg
+}
+
+type neighbours struct {
+	current int
+	n       []*graph.V
+}
+
+func (a *relabelToFrontAlgo) do(s, t *graph.V) map[graph.Edge]int {
+	l := list.New()
+
+	for _, v := range a.ctx.g.Vertexes {
+		if v != s && v != t {
+			l.PushBack(v)
+			a.allN[v].current = 0
+		}
+	}
+
+	u := l.Front()
+
+	for u != nil {
+		uVert := u.Value.(*graph.V)
+
+		oldH := a.ctx.h[uVert]
+		a.discharge(uVert)
+		if a.ctx.h[uVert] > oldH {
+			l.MoveToFront(u)
+		}
+		u = u.Next()
+	}
+	return a.ctx.f
+}
+
+func (a *relabelToFrontAlgo) discharge(u *graph.V) {
+	for a.ctx.e[u] > 0 {
+		var v *graph.V
+		if a.allN[u].current >= len(a.allN[u].n) {
+			v = nil
+		} else {
+			v = a.allN[u].n[a.allN[u].current]
+		}
+
+		if v == nil {
+			relabel(&a.ctx, u)
+			a.allN[u].current = 0
+		} else if a.ctx.c[graph.Edge{u, v}]-a.ctx.f[graph.Edge{u, v}] > 0 && a.ctx.h[u] == (a.ctx.h[v]+1) {
+			push(&a.ctx, u, v)
+		} else {
+			a.allN[u].current += 1
+		}
+	}
+}
+
+func initPreflow(ctx *context, g *graph.G, s *graph.V) {
+	for e, w := range g.Weights {
+		ctx.c[e] = w
+		ctx.f[e] = 0
+		ctx.f[graph.Edge{e.V2, e.V1}] = 0
+	}
+
+	for _, vert := range g.Vertexes {
+		ctx.h[vert] = 0
+		ctx.e[vert] = 0
+	}
+
+	for _, u := range g.Adj[s] {
+		ctx.f[graph.Edge{s, u}] = ctx.c[graph.Edge{s, u}]
+		ctx.f[graph.Edge{u, s}] = -ctx.c[graph.Edge{s, u}]
+		ctx.e[u] = ctx.c[graph.Edge{s, u}]
+		ctx.e[s] -= ctx.c[graph.Edge{s, u}]
+	}
+	ctx.h[s] = len(g.Vertexes)
 }
--- graph/flow/push_test.go
+++ graph/flow/push_test.go
@@ -6,56 +6,136 @@ import (
 	"testing"
 )
 
-var (
-	source = &graph.V{Val: "source"}
-	a      = &graph.V{Val: "a"}
-	b      = &graph.V{Val: "b"}
-	cc     = &graph.V{Val: "c"}
-	d      = &graph.V{Val: "d"}
-	sink   = &graph.V{Val: "sink"}
-)
-
-var gr2 = graph.G{
-	Vertexes: []*graph.V{source, b, a, cc, d, sink},
-	Adj: map[*graph.V][]*graph.V{
-		source: {a, b},
-		a:      {cc, sink},
-		b:      {a},
-		cc:     {d},
-		d:      {sink},
-		sink:   {},
-	},
-	Weights: map[graph.Edge]int{
-		{source, a}: 13,
-		{source, b}: 10,
-		{a, cc}:     6,
-		{a, sink}:   7,
-		{b, a}:      3,
-		{cc, d}:     10,
-		{d, sink}:   5,
-	},
+func TestPushRelabelMaxFlow(t *testing.T) {
+	testAlg(t, PushRelabelMaxFlow)
 }
 
-func TestPushRelabelMaxFlow(test *testing.T) {
-	result := PushRelabelMaxFlow(&gr, s, t)
-
-	assert.Equal(test, result[graph.Edge{t, v3}], -19)
-	assert.Equal(test, result[graph.Edge{t, v4}], -4)
-	assert.Equal(test, result[graph.Edge{v3, v4}], -7)
-	assert.Equal(test, result[graph.Edge{v3, v1}], -12)
-	assert.Equal(test, result[graph.Edge{v4, v2}], -11)
-	assert.Equal(test, result[graph.Edge{v1, s}], -12)
-	assert.Equal(test, result[graph.Edge{v2, s}], -11)
+func TestRelabelToFront(t *testing.T) {
+	testAlg(t, RelabelToFront)
 }
 
-func TestPushRelabelMaxFlow2(test *testing.T) {
-	result := PushRelabelMaxFlow(&gr2, source, sink)
+func testAlg(t *testing.T, algo func(g *graph.G, s *graph.V, t *graph.V) map[graph.Edge]int) {
+	{
+		var (
+			source = &graph.V{Val: "source"}
+			a      = &graph.V{Val: "a"}
+			b      = &graph.V{Val: "b"}
+			c      = &graph.V{Val: "c"}
+			d      = &graph.V{Val: "d"}
+			sink   = &graph.V{Val: "sink"}
+		)
 
-	assert.Equal(test, result[graph.Edge{sink, d}], -5)
-	assert.Equal(test, result[graph.Edge{sink, a}], -7)
-	assert.Equal(test, result[graph.Edge{d, cc}], -5)
-	assert.Equal(test, result[graph.Edge{cc, a}], -5)
-	assert.Equal(test, result[graph.Edge{a, b}], -3)
-	assert.Equal(test, result[graph.Edge{a, source}], -9)
-	assert.Equal(test, result[graph.Edge{b, source}], -3)
+		var gr = graph.G{
+			Vertexes: []*graph.V{source, b, a, c, d, sink},
+			Adj: map[*graph.V][]*graph.V{
+				source: {a, b},
+				a:      {c, sink},
+				b:      {a},
+				c:      {d},
+				d:      {sink},
+				sink:   {},
+			},
+			Weights: map[graph.Edge]int{
+				{source, a}: 13,
+				{source, b}: 10,
+				{a, c}:      6,
+				{a, sink}:   7,
+				{b, a}:      3,
+				{c, d}:      10,
+				{d, sink}:   5,
+			},
+		}
+
+		result := algo(&gr, source, sink)
+
+		assert.Equal(t, result[graph.Edge{sink, d}], -5)
+		assert.Equal(t, result[graph.Edge{sink, a}], -7)
+		assert.Equal(t, result[graph.Edge{d, c}], -5)
+		assert.Equal(t, result[graph.Edge{c, a}], -5)
+		assert.Equal(t, result[graph.Edge{a, b}], -3)
+		assert.Equal(t, result[graph.Edge{a, source}], -9)
+		assert.Equal(t, result[graph.Edge{b, source}], -3)
+	}
+
+	{
+		var (
+			source = &graph.V{Val: "source"}
+			a      = &graph.V{Val: "a"}
+			b      = &graph.V{Val: "b"}
+			c      = &graph.V{Val: "c"}
+			d      = &graph.V{Val: "d"}
+			sink   = &graph.V{Val: "sink"}
+		)
+
+		var gr = graph.G{
+			Vertexes: []*graph.V{source, sink, a, b, c, d},
+			Adj: map[*graph.V][]*graph.V{
+				source: {a, b},
+				a:      {c},
+				b:      {a, d},
+				c:      {b, sink},
+				d:      {c, sink},
+				sink:   {},
+			},
+			Weights: map[graph.Edge]int{
+				{source, a}: 16,
+				{source, b}: 13,
+				{a, c}:      12,
+				{b, a}:      4,
+				{b, d}:      14,
+				{c, b}:      9,
+				{c, sink}:   20,
+				{d, c}:      7,
+				{d, sink}:   4,
+			},
+		}
+
+		result := algo(&gr, source, sink)
+
+		assert.Equal(t, result[graph.Edge{sink, c}], -19)
+		assert.Equal(t, result[graph.Edge{sink, d}], -4)
+		assert.Equal(t, result[graph.Edge{c, d}], -7)
+		assert.Equal(t, result[graph.Edge{c, a}], -12)
+		assert.Equal(t, result[graph.Edge{d, b}], -11)
+		assert.Equal(t, result[graph.Edge{a, source}], -12)
+		assert.Equal(t, result[graph.Edge{b, source}], -11)
+	}
+
+	{
+		var (
+			source = &graph.V{Val: "source"}
+			a      = &graph.V{Val: "a"}
+			b      = &graph.V{Val: "y"}
+			c      = &graph.V{Val: "c"}
+			sink   = &graph.V{Val: "sink"}
+		)
+
+		var gr = graph.G{
+			Vertexes: []*graph.V{source, a, b, c, sink},
+			Adj: map[*graph.V][]*graph.V{
+				source: {a, b},
+				a:      {b, sink},
+				b:      {c},
+				c:      {sink, a},
+				sink:   {},
+			},
+			Weights: map[graph.Edge]int{
+				{source, a}: 12,
+				{source, b}: 14,
+				{a, b}:      5,
+				{a, sink}:   16,
+				{b, c}:      8,
+				{c, sink}:   10,
+				{c, a}:      7,
+			},
+		}
+
+		result := algo(&gr, source, sink)
+		assert.Equal(t, result[graph.Edge{a, source}], -12)
+		assert.Equal(t, result[graph.Edge{b, source}], -8)
+		assert.Equal(t, result[graph.Edge{c, b}], -8)
+		assert.Equal(t, result[graph.Edge{sink, c}], -8)
+		assert.Equal(t, result[graph.Edge{sink, a}], -12)
+
+	}
 }
